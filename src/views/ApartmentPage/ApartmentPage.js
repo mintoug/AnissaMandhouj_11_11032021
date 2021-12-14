c70466387d8c89c9f30e72051c4a27a91be12629
Mandhouj A <nouitou@yahoo.fr>
2021-12-14 10:03:40 +0100

    ApartmentPage

--- src/views/ApartmentPage/ApartmentPage.js
+++ src/views/ApartmentPage/ApartmentPage.js
@@ -1,8 +1,12 @@
 import React, {Component} from 'react';
 import './ApartmentPage.css';
 import Title from '../../components/Title/Title';
-// import Tag from '../../components/Tag/Tag';
-import {data} from '../../datas/data'
+import Tag from '../../components/Tag/Tag';
+import Local from '../../components/Local/Local';
+import Profil from '../../components/Profil/Profil';
+import Collaps from '../../components/Collaps/Collaps'
+import Rating from '../../components/Rating/Rating';
+import Slides from '../../components/Slides/Slides';
  class ApartmentPage extends Component {
     constructor(props) {
         super(props)
@@ -12,40 +16,52 @@ import {data} from '../../datas/data'
     }
 
     getApartment = () => {
-        const apartment = data.filter(
+        const apartment = this.props.apartments.filter(
             (apt) => apt.id === this.props.match.params.id
         )
 
-        return apartment
+        return apartment[0]
     }
 
-    // getTags = () => {
-    //     return (
-    //         <div className="apartmentTagsBox">
-    //             {this.state.apartment.tags.map((tag, index) => (
-    //                 <Tag tag={tag} key={index} />
-    //             ))}
-    //         </div>
-    //     )
-    // }
+    getTags = () => {
+        return (
+            <div className="apartmentTagsBox">
 
+                {this.state.apartment.tags.map((tag, index) => (
+                    <Tag tag={tag} key={index} />
+                ))}
+            </div>
+        )
+    }
 
+    getCollapsibles = () => {
+        return (
+            <section className="apartmentCollapsible">
+                <Collaps title='Description' content={this.state.apartment.description}/>
+                <Collaps title='Équipements' content={this.state.apartment.equipments}/>
+            </section>
+        )
+    }
  
     render() {
 
         return (
             
             <main>
-            
-
+                   <Slides pictures={this.state.apartment.pictures}/>
+                <section className="apartmentInformations">
                     <div className="apartmentBox">
-                        <Title title={this.state.apartment.title}/>
-                        {/* {this.getTags()} */}
-                        </div>
-                     <h1>hello</h1>
-                    
-                    
-            
+                        <Title title = {this.state.apartment.title}/>
+                        <Local location = {this.state.apartment.location}/>
+                        {this.getTags()}
+                    </div>
+                    <div className="apartmentBoxAside">
+                        <Profil host = {this.state.apartment.host}/>
+                        <Rating rating={this.state.apartment.rating}/>
+                        
+                    </div>
+                </section>
+                {this.getCollapsibles()}
                 
             </main>
         )
